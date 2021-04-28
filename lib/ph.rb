e6ac1658f2bd70abf15ab7bf4fac96944a3122f1
elcuervo <elcuervo@elcuervo.net>
2021-04-28 18:32:39 -0300

    Adds conversion helpers

--- lib/ph.rb
+++ lib/ph.rb
@@ -6,12 +6,38 @@ class PH
   NotGreyscaleError = Class.new(StandardError)
   IncorrectDimensionsError = Class.new(StandardError)
 
-  def self.hash(pixels_2d)
-    new(pixels_2d).hash
-  end
+  class << self
+    def hash(pixels_2d)
+      new(pixels_2d).hash
+    end
 
-  def self.vector(pixels_2d)
-    new(pixels_2d).vector
+    def vector(pixels_2d)
+      new(pixels_2d).vector
+    end
+
+    def vector_to_hash(vector)
+      vector.join.to_i(2).to_s(16)
+    end
+
+    def hash_to_vector(hash)
+      hash.hex.to_s(2).split("").map(&:to_i)
+    end
+
+    def distance(a, b)
+      a = a.kind_of?(Array) ? vector_to_hash(a) : a
+      b = b.kind_of?(Array) ? vector_to_hash(b) : b
+
+      n = a.hex ^ b.hex
+
+      ones = 0
+
+      while n > 0
+        n &= n - 1
+        ones += 1
+      end
+
+      ones
+    end
   end
 
   def initialize(pixels_2d)
@@ -24,7 +50,7 @@ class PH
 
   def hash
     # Binary to hex string
-    vector.join.to_i(2).to_s(16)
+    PH.vector_to_hash(vector)
   end
 
   def vector
--- spec/ph_spec.rb
+++ spec/ph_spec.rb
@@ -4,30 +4,64 @@ describe PH do
   let(:file) { "spec/image.jpg" }
   let(:size) { 64 }
 
-  before do
-    img = Vips::Image.new_from_file(file)
-    width, height = img.width, img.height
-    w_scale, v_scale = size.fdiv(width), size.fdiv(height)
+  describe "hashing an image" do
+    before do
+      img = Vips::Image.new_from_file(file)
+      width, height = img.width, img.height
+      w_scale, v_scale = size.fdiv(width), size.fdiv(height)
 
-    # Rescale to 64x64 and greyscale
-    img = img.resize(w_scale, vscale: v_scale).colourspace(:grey16)
-    # Ensure a 2D plane
-    @pixels_2d = img.to_a.map(&:flatten)
+      # Rescale to 64x64 and greyscale
+      img = img.resize(w_scale, vscale: v_scale).colourspace(:grey16)
+      # Ensure a 2D plane
+      @pixels_2d = img.to_a.map(&:flatten)
+    end
+
+    subject { PH.new(@pixels_2d) }
+
+    it "#hash" do
+      assert_equal "859091ce633aaebb", subject.hash
+    end
+
+    it "#vector" do
+      vector = [
+        1, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1,
+        0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0,
+        1, 1
+      ]
+
+      assert_equal vector, subject.vector
+    end
   end
 
-  subject { PH.new(@pixels_2d) }
+  describe "comparing hashes" do
+    let(:hash_a) { "859091ce633aaebb" }
+    let(:hash_b) { "859091ce633aaeba" }
 
-  it "#hash" do
-    assert_equal "859091ce633aaebb", subject.hash
-  end
+    let(:vector_a) do
+      [
+        1, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1,
+        0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0,
+        1, 1
+      ]
+    end
 
-  it "#vector" do
-    vector = [
-      1, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1,
-      0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0,
-      1, 1
-    ]
+    let(:vector_b) do
+      [
+        1, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1,
+        0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0,
+        1, 0
+      ]
+    end
 
-    assert_equal vector, subject.vector
+    it "conversion" do
+      assert_equal vector_a, PH.hash_to_vector(hash_a)
+      assert_equal hash_b, PH.vector_to_hash(vector_b)
+    end
+
+    it "#distance" do
+      assert_equal 0, PH.distance(vector_b, vector_b)
+      assert_equal 0, PH.distance(hash_a, hash_a)
+      assert_equal 1, PH.distance(hash_a, vector_b)
+    end
   end
 end
